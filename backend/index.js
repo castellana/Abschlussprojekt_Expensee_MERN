393e7131ea4eec224c73d87ef0a3143f3d8cd7b6
castellana <r_aquel@hotmail.com>
2021-02-05 16:34:42 +0100

    connection back with front, cors added, component 'Transactions'

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ require('dotenv').config({path: './config/.env'})
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 
 const transactionsRoute = require('./routes/transactionsRoute');
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
---
+++ frontend/src/components/Transactions.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+// import { Link } from "react-router-dom"
+const axios = require('axios').default
+
+const Transactions = () => {
+    const [data, setData] = useState(undefined) //el valor inicial le ponemos undefined
+    useEffect(() => { 
+        axios
+            .get('http://localhost:5000/transactions')
+            .then(transactions => {
+                console.log(transactions)
+                setData(transactions.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+    return ( 
+        <>
+            <h2>Hier sind deine letzte Geldtransaktionen</h2>
+            {/* {JSON.stringify(data)} probamos si nos da algo */}
+            {data !== undefined ?
+                data.map(transaction => <div key={transaction._id}>
+                    <h3>{transaction.title}</h3>
+                    <p>{transaction.amount}</p>
+                    <p>{transaction.category}</p>
+                    <p>{transaction.date}</p>
+
+                    {/* <Link to={`/contact/${contact._id}`}> Klicke hier </Link> */}
+                </div>)
+                : "loading"}
+        </>
+     );
+}
+ 
+export default Transactions;
